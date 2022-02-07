cb6f083e0741562f346cdaccba3a1ef1d0637027
Julian <julian_sche@yahoo.de>
2022-02-07 22:07:52 +0100

    Submodule and Simulation Update

--- generated/c8b10b_to_udp_test/usr/testbench.cpp
+++ generated/c8b10b_to_udp_test/usr/testbench.cpp
@@ -34,11 +34,17 @@ int main(int argc, char **argv) {
     uint8_t data[] = {
         0xBC, 0x02, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 
         0xBC, 0x32, 0x11, 0x12, 0x02, 0x03, 0x04, 0x05, 
+        /*0xAB, 0xAA, 0x55, 0x66, 0x88, 0x71, 0x72, 0x73, 
+        0x41, 0x42, 0x43, 0x77, 0x44, 0x48, 0x71, 0x25, 
+        0x91, 0x90, 0x89, 0x65, 0x89, 0x15, 0x27, 0x46, */
         0xBC, 0x02, 0x10, 0x21, 0x22, 0xAB, 0xCD, 0xFB
     };
     uint8_t isk[]= {
         0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        /*0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,*/
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
     };
 
@@ -86,10 +92,7 @@ int main(int argc, char **argv) {
                 printf("Do Test, first = %i second = %i jump = %i \n", first, second, jump);
                 myfile << "Test State: First=" << std::to_string(first) << ", Second=" << std::to_string(second) << ", Jump=" << std::to_string(jump) << std::endl;
                 create_testdata(first ,second ,jump ,data ,isk ,sizeof(data) ,&c8b10b_intf );
-                for(uint16_t i = 0; i < 700; i++){
-                    if(i > 700){
-                        break;
-                    }
+                for(uint16_t i = 0; i < 350; i++){
                     eth_intf.tick();
                     mem_intf.tick();
                     c8b10b_intf.deq_rx_fifo();
@@ -106,7 +109,7 @@ int main(int argc, char **argv) {
                 }
 
 
-                for(uint16_t i = 0; i < 700; i++){
+                for(uint16_t i = 0; i < 350; i++){
                     eth_intf.tick();
                     mem_intf.tick();
                     c8b10b_intf.deq_rx_fifo();
@@ -125,6 +128,75 @@ int main(int argc, char **argv) {
         }
     }
 
+    uint8_t data2[] = {
+        0xBC, 0x02, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 
+        0xBC, 0x32, 0x11, 0x12, 0x02, 0x03, 0x04, 0x05, 
+        0xAB, 0xAA, 0x55, 0x66, 0x88, 0x71, 0x72, 0x73, 
+        0x41, 0x42, 0x43, 0x77, 0x44, 0x48, 0x71, 0x25, 
+        0x91, 0x90, 0x89, 0x65, 0x89, 0x15, 0x27, 0x46, 
+        0xBC, 0x02, 0x10, 0x21, 0x22, 0xAB, 0xCD, 0xFB
+    };
+    uint8_t isk2[]= {
+        0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+    };
+
+    printf("\n Trigger at Start and Stop Symbol \n");
+    myfile << std::endl;
+    myfile << "Trigger at Start and Stop Symbol" << std::endl;
+    countmgmt = 0;
+    for(uint8_t first = 0; first < 4; first++){
+        tb->m_core->io_first = first;
+        for(uint8_t second = 0; second < 5; second++){
+            tb->m_core->io_second = second;
+            for(uint8_t jump = 0; jump < 20; jump++){
+                tb->m_core->io_jump = jump;
+                countmgmt = 0;
+                printf("Do Test, first = %i second = %i jump = %i \n", first, second, jump);
+                myfile << "Test State: First=" << std::to_string(first) << ", Second=" << std::to_string(second) << ", Jump=" << std::to_string(jump) << std::endl;
+                create_testdata(first ,second ,jump ,data2 ,isk2 ,sizeof(data2) ,&c8b10b_intf );
+                for(uint16_t i = 0; i < 350; i++){
+                    /*if(i > 700){
+                        break;
+                    }*/
+                    eth_intf.tick();
+                    mem_intf.tick();
+                    c8b10b_intf.deq_rx_fifo();
+                    eth_intf.clear_rx_fifo();
+                    tb->tick();
+                    if(!eth_intf.fifo_mgmt.empty()){
+                        if(countmgmt >= 2){
+                            check_testdata(&data2[1], &isk2[1], sizeof(data2)-2, &eth_intf, &myfile);
+                        }else{
+                            check_testdata(&data2[1], &isk2[1], sizeof(data2)-2-jump, &eth_intf, &myfile);
+                        }
+                        countmgmt++;
+                    }
+                }
+
+
+                for(uint16_t i = 0; i < 350; i++){
+                    eth_intf.tick();
+                    mem_intf.tick();
+                    c8b10b_intf.deq_rx_fifo();
+                    eth_intf.clear_rx_fifo();
+                    tb->tick();
+                    if(!eth_intf.fifo_mgmt.empty()){
+                        if(countmgmt >= 2){
+                            check_testdata(&data2[1], &isk2[1], sizeof(data2)-2, &eth_intf, &myfile);
+                        }else{
+                            check_testdata(&data2[1], &isk2[1], sizeof(data2)-2-jump, &eth_intf, &myfile);
+                        }
+                        countmgmt++;
+                    }
+                }
+            }
+        }
+    }
 
 }
 
